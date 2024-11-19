e824aad6e19a044880227ec51584e23b32bba3d7
Daniel J Brown <browndj3@gmail.com>
2024-11-19 13:55:20 -0600

    Adjusting type hinting and logic
    - Correcting some type hinting that was lacking or not broad enough
    - Removed endpoints using the old request style
    - Removing pyrate_limiter as it no longer matches the scope of this
    project

--- src/new_traders/primitive.py
+++ src/new_traders/primitive.py
@@ -1,10 +1,10 @@
 import asyncio
-from typing import Final, TypeVar
+from types import TracebackType
+from typing import Any, Final, TypeVar
 # TODO: add .env support
 
 from niquests import AsyncSession
 from niquests._typing import QueryParameterType
-from pyrate_limiter import Duration, Limiter, InMemoryBucket, Rate
 
 from . import model
 
@@ -23,20 +23,18 @@ class SpaceTraders:
             },
         )
 
-        self.rates: list[Rate] = [
-            Rate(2, Duration.SECOND),
-            Rate(30, Duration.MINUTE),
-        ]
-        self.bucket: InMemoryBucket = InMemoryBucket(self.rates)
-        self.limiter: Limiter = Limiter(self.bucket)
-
     async def close(self):
         await self.session.close()
 
     async def __aenter__(self):
         return self
 
-    async def __aexit__(self, exc_type, exc_value, traceback):
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_value: BaseException | None,
+        traceback: TracebackType | None,
+    ) -> bool | None:
         await self.close()
 
     async def _request(
@@ -46,10 +44,14 @@ class SpaceTraders:
         url: str,
         *,
         params: QueryParameterType | None = None,
-        data: dict[str, int | float | str] | None = None,
+        data: dict[str, Any] | None = None,
     ) -> T:
-        self.limiter.try_acquire(url)
-        response = await self.session.request(method, url, params=params, json=data)
+        response = await self.session.request(
+            method,
+            url,
+            params=params,
+            json=data,
+        )
         json_data = await response.json()
         return response_model(**json_data)
 
@@ -83,66 +85,6 @@ class SpaceTraders:
             data=data,
         )
         return agent
-
-    async def get_agent(self) -> model.Agent:
-        url = f"{self.BASE_URL}/my/agent"
-        self.limiter.try_acquire("/my/agent")
-        async with self.session as session:
-            response = await session.get(url)
-            return response.json()
-
-    async def get_agents(
-        self,
-        limit: int = 10,
-        page: int = 1,
-    ) -> model.ListAgentsResponse:
-        url = f"{self.BASE_URL}/agents"
-        self.limiter.try_acquire("/agents")
-        async with self.session as session:
-            response = await session.get(
-                url,
-                data={
-                    "limit": limit,
-                    "page": page,
-                },
-            )
-            return response.json()
-
-    async def get_public_agent(self, agent_symbol: str = "FEBA66") -> model.Agent:
-        url = f"{self.BASE_URL}/agents/f{agent_symbol}"
-        self.limiter.try_acquire(f"/agents/{agent_symbol}")
-        async with self.session as session:
-            response = await session.get(
-                url,
-                data={"agentSymbol": agent_symbol},
-            )
-            return response.json()
-
-    async def list_contracts(
-        self,
-        limit: int = 10,
-        page: int = 1,
-    ) -> model.ListContractsResponse:
-        url = f"{self.BASE_URL}/my/contracts"
-        self.limiter.try_acquire("/my/contracts")
-        async with self.session as session:
-            response = await session.get(
-                url,
-                data={
-                    "limit": limit,
-                    "page": page,
-                },
-            )
-            return response.json()
-
-    async def get_contract(self, contract_id: str) -> model.Contract:
-        url = f"{self.BASE_URL}/my/contracts/{contract_id}"
-        self.limiter.try_acquire(f"/my/contracts/{contract_id}")
-        async with self.session as session:
-            response = await session.get(url)
-            return response.json()
-
-    async def accept_contract(self, contract_id: str): ...
 
     def update_token(self, token: str) -> None:
         self.token = token
