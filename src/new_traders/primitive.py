9ca0d822382a606780070f0b5dc4745c7fd75601
Daniel J Brown <browndj3@gmail.com>
2024-11-21 17:05:41 -0600

    Added error map and added error raising logic

--- src/new_traders/primitive.py
+++ src/new_traders/primitive.py
@@ -1,10 +1,12 @@
-import asyncio
 from types import TracebackType
-from typing import Any, Final, TypeVar
-# TODO: add .env support
+from typing import Any, TypeVar
 
 from niquests import AsyncSession
 from niquests._typing import QueryParameterType
+from niquests.exceptions import JSONDecodeError as RequestsJSONDecodeError
+from niquests.models import Response
+
+from new_traders.errors import ERROR_MAPPING, SpaceTradersAPIError
 
 from . import model
 
@@ -12,16 +14,12 @@ T = TypeVar("T")
 
 
 class SpaceTraders:
-    def __init__(self, token: str) -> None:
-        self.BASE_URL: Final = "https://api.spacetraders.io/v2"
-        self.session: AsyncSession = AsyncSession()
-        self.token: str = token
-        self.session.headers.update(
-            {
-                "Authorization": f"Bearer {self.token}",
-                "Accept": "application/json",
-            },
-        )
+    def __init__(self, token: str | None) -> None:
+        self.session: AsyncSession = AsyncSession(base_url="https://api.spacetraders.io/v2")
+        self.token = token
+        self.session.headers.update({"Accept": "application/json"})
+        if self.token:
+            self.session.headers.update({"Authorization": f"Bearer {self.token}"})
 
     async def close(self):
         await self.session.close()
@@ -52,16 +50,38 @@ class SpaceTraders:
             params=params,
             json=data,
         )
+        if not response.ok:
+            self._handle_error(response)
         json_data = await response.json()
         return response_model(**json_data)
 
+    def _handle_error(self, response: Response):
+        response_json = {}
+        try:
+            response_json = response.json()
+        except RequestsJSONDecodeError:
+            response.raise_for_status()
+
+        error = response_json["error"]
+        code = error["code"]
+        message = error["message"]
+        error_type = ERROR_MAPPING.get(code)
+
+        try:
+            response.raise_for_status()
+        except Exception as e:
+            if not error_type:
+                raise SpaceTradersAPIError(code, message, response_json) from e
+            raise error_type(code, message, response_json) from e
+
+
+
     async def get_status(self) -> model.ServerStatsResponse:
         """Fetch the current status of the server."""
-        url = f"{self.BASE_URL}/"
         server_status = await self._request(
             model.ServerStatsResponse,
             "GET",
-            url,
+            "/",
         )
         return server_status
 
@@ -72,7 +92,6 @@ class SpaceTraders:
         email: str,
     ) -> model.RegisterNewAgentResponse:
         """Creates a new agent and ties it to an account."""
-        url = f"{self.BASE_URL}/register"
         data = {
             "faction": faction,
             "symbol": agent_symbol,
@@ -81,7 +100,7 @@ class SpaceTraders:
         agent = await self._request(
             model.RegisterNewAgentResponse,
             "POST",
-            url,
+            "/register",
             data=data,
         )
         return agent
